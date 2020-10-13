bc5170e29970704ed52ca68674819e7bcf3aae82
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-10-13 11:14:00 -0300

    pagina com mapa

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -22,7 +22,7 @@ function Landing() {
           <span>São Paulo</span>
         </div>
 
-        <Link to="/" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6" />
         </Link>
       </div>
