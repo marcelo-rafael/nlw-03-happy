913e2e4e96ab2a1e6db23f67edb4c42c034d8857
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-10-16 16:37:58 -0300

    conectando front com back

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../../images/map-marker.svg';
-// import mapIcon from '../../utils/mapIcon';
 
 import { PageMap } from './styles';
 
